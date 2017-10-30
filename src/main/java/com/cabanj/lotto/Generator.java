9ae4df0567ff6f424419a7fdd8a2fb4c4d9d5639
cabanj <jacek.caban@contractors.roche.com>
2017-10-30 13:31:21 +0100

    count added for number of generated numbers

--- src/main/java/com/cabanj/lotto/Generator.java
+++ src/main/java/com/cabanj/lotto/Generator.java
@@ -9,17 +9,22 @@ public class Generator {
      * Default limit is 50 since numbers range is 1-49
      */
     private int limit = 50;
+    private int count = 6;
 
+    /**
+     * Construct with default values 6 numbers up to 49
+     */
     public Generator(){}
 
-    public Generator(int limit) {
+    public Generator(int limit, int count) {
         this.limit = limit;
+        this.count = count;
     }
 
     private Set<Integer> generate(){
         Set<Integer> numbers = new TreeSet<>();
         Random random = new Random();
-        for(int i = 0; i < 6; i++){
+        for(int i = 0; i < count; i++){
             numbers.add(random.nextInt(limit));
         }
         return numbers;
